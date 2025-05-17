86a351e04daab6ba6df01f3e3c71b8189021de76
urooj haider <suhz1100@gmail.com>
2025-05-17 18:22:46 +0500

    add

---
+++ js.js
@@ -0,0 +1,20 @@
+let username 
+let myName = "haider zaidi";
+let message
+message = "Hellow World";
+alert(message);
+let sutdentName = "Obaid";
+alert(sutdentName);
+let studentAge = "28 Years Old";
+alert(studentAge);
+let studentEducaiton = "Certified Mobile Application Developer";
+alert(studentEducaiton);
+let str = "Pizza";
+alert("Pizza\nPizz\nPiz\nPi\nP");
+let email = ("example@example.com");
+alert("My email address is "+ email);
+let book = "A smarter way to learn JavaScript"
+alert("I am trying to learn from book " + book);
+alert("==========")
+
+
